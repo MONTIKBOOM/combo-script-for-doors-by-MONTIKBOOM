ecb8b0c6cdf7a0282295cab309dfb8aa2a0289ba
MONTIKBOOM <129415993+MONTIKBOOM@users.noreply.github.com>
2023-04-02 09:06:15 +0300

    Update good script combo for doors.lua

--- good script combo for doors.lua
+++ good script combo for doors.lua
@@ -517,3 +517,19 @@ task.wait(2)
 
 -- Infinite Yield
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"),true))()
+
+--Anti Глаза
+
+local mt = getrawmetatable(game);
+local old = mt.__namecall
+setreadonly(mt,false)
+mt.__namecall = newcclosure(function(remote,...)
+   args = {...}
+   method = tostring(getnamecallmethod())
+   if method == "FireServer" and tostring(remote) == "MotorReplication" then
+       args[2] = 90
+       return old(remote,unpack(args))
+   end
+   return old(remote,...)
+end)
+setreadonly(mt,true)
