7f8b7a8b0f809676cc08f2fb026bf82c3dcb0c94
MONTIKBOOM <129415993+MONTIKBOOM@users.noreply.github.com>
2023-04-04 13:25:40 +0300

    Update good script combo for doors.lua

--- good script combo for doors.lua
+++ good script combo for doors.lua
@@ -1,3 +1,23 @@
+
+-- Infinite Yield
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"),true))()
+
+--Anti Eyes
+
+local mt = getrawmetatable(game);
+local old = mt.__namecall
+setreadonly(mt,false)
+mt.__namecall = newcclosure(function(remote,...)
+   args = {...}
+   method = tostring(getnamecallmethod())
+   if method == "FireServer" and tostring(remote) == "MotorReplication" then
+       args[2] = 90
+       return old(remote,unpack(args))
+   end
+   return old(remote,...)
+end)
+setreadonly(mt,true)
+
 -- bypass noclip
 
 _G.Keybind = "R"
@@ -515,21 +535,4 @@ OrionLib:Init()
  
 task.wait(2)
 
--- Infinite Yield
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"),true))()
-
---Anti Глаза
-
-local mt = getrawmetatable(game);
-local old = mt.__namecall
-setreadonly(mt,false)
-mt.__namecall = newcclosure(function(remote,...)
-   args = {...}
-   method = tostring(getnamecallmethod())
-   if method == "FireServer" and tostring(remote) == "MotorReplication" then
-       args[2] = 90
-       return old(remote,unpack(args))
-   end
-   return old(remote,...)
-end)
-setreadonly(mt,true)
+--By MONTIKBOOM
